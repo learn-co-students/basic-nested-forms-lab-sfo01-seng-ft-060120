f20fd2d807a1de99f4a1b09ea9606f7066894033
keeks5456 <Akikogreen08@gmail.com>
2020-06-28 13:23:54 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
 end
+
+# Define a conventional association writer for the primary model to properly instantiate associations based on the nested params association data.
